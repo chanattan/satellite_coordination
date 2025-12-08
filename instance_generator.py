f93f90542de7c4f85fec308f2cb22ed65803e1c0
chanattan <sok.chanattan@gmail.com>
2025-12-08 16:06:34 +0100

    first structure of the problem, instance generation TODO, instance model TODO.

---
+++ instance_generator.py
@@ -0,0 +1,19 @@
+def generate_ESOP_instance(nb_satellites, nb_users, nb_tasks):
+    """
+    Étant donné un nombre de satellites nb_satellites, nombre d'utilisateurs exclusifs nb_users et un nombre de tâches nb_tasks,
+    cette fonction génère et retourne une instance aléatoire I du problème ESOP.
+
+    L'instance est modélisée par un objet (classe Python) et contient également un format texte pour l'exportation.
+    Le format texte utilisé pour une instance est le suivant :
+
+        [Parameters]
+        Satellites : nb_satellites
+        Exlusive users : nb_users
+        Tasks : nb_tasks
+
+        []
+    """
+    pass
+
+def generate_DCOP_instance(instance):
+    pass
